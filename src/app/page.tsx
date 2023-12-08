1bc02a78bbed4baa5e6fc0691a26871d778b9979
aParrot <a.lapenas04@gmail.com>
2023-12-08 13:05:18 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <nav></nav>
+      <h1 className="text-5xl">Tittel</h1>
+    </main>
+  );
+}
